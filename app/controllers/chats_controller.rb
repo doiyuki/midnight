40e98b611eaa2a0c171d8ad72f48f01a8d3c4f69
yukidoi <kurouhougan.b@gmail.com>
2015-04-28 20:17:39 +0900

    Fix chat view temporarily

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,5 @@
 class ChatsController < ApplicationController
   def index
-    # TODO ユーザー名入力させる
-    @user_name = 'testing user ex'
     @room_name = $program_name
     gon.room_id = @room_name
   end
