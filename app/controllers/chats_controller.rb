50bae2096b8aafba17ea76e41f49d1785390771d
yukidoi <kurouhougan.b@gmail.com>
2015-04-30 17:02:45 +0900

    Fix port to heroku

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,5 +2,6 @@ class ChatsController < ApplicationController
   def index
     @room_name = $program_name
     gon.room_id = @room_name
+    @port = Rails.env == 'production' ? $PORT : 3000
   end
 end
